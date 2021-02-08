0f3ff21c5368fb1dd55f60ff8a8d7dd989c9ac81
apolonskyi <apolonskyi@alteryx.com>
2021-02-08 21:42:24 +0200

    handle mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,4 +4,4 @@ mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
      useUnifiedTopology: true,
      useFindAndModify: false
-    });
+    }).catch(error => console.log(error));
