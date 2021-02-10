79c1b4a826a9c82b00673c3b518b656ec9ba41ab
apolonskyi <apolonskyi@alteryx.com>
2021-02-10 20:35:28 +0200

    cors2

--- src/app.js
+++ src/app.js
@@ -9,6 +9,10 @@ app.use(express.json());
 app.use(usersRouter);
 app.use(tasksRouter);
 app.use(require('cors')({ credentials: true, origin: true }));
+app.all('*', (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://localhost:3000");
+    next();
+});
 
 
 module.exports = app;
